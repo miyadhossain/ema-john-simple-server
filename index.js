6b2df72e2db123318f571291569dccac89474f40
miyadhossain <miyad.mh@gmail.com>
2021-03-27 23:20:29 +0600

    add port and nodemon fixed

--- index.js
+++ index.js
@@ -66,6 +66,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Ema-John-Server at http://localhost:${port}`);
 });
